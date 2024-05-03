ea694562e1197e06779ae13d8880bcc455b5b00c
Juniper Watson <lyravox1@gmail.com>
2024-05-02 22:43:43 -0500

    Edit app.py

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 # app.py
 
 from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-messages = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), nullable=False)
 
 @app.route('/')
 def index():
+    messages = Message.query.all()
     return render_template('home.html', messages=messages)
 
 @app.route('/post_message', methods=['POST'])
 def post_message():
     text = request.form['message']
-    messages.append(text)
+    new_message = Message(text=text)
+    db.session.add(new_message)
+    db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
