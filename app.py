2a277d16cbd6a3cd9c468863d8cbc7ca5bac3d72
Juniper Watson <lyravox1@gmail.com>
2024-05-02 22:37:13 -0500

    Edit app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 # app.py
+
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+messages = []
+
+@app.route('/')
+def index():
+    return render_template('home.html', messages=messages)
+
+@app.route('/post_message', methods=['POST'])
+def post_message():
+    text = request.form['message']
+    messages.append(text)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
